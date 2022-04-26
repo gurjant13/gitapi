cf5125f9188d4bc86656fbb615f4d77e2b391849
gurjant13 <gurjantsinghsidhu@ymail.com>
2022-04-26 14:39:48 +1200

    Initial Commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,156 @@
-import logo from './logo.svg';
+import { React, useState, useEffect, Component } from 'react';
+import { Container, Form, Row, Label, InputGroup, Col, FormControl, Button } from 'react-bootstrap';
 import './App.css';
+import axios from 'axios';
 
 function App() {
+
+
+  const [user, setUser] = useState("");
+  const [record, setRecord] = useState("");
+  const [repos, setRepos] = useState("");
+
+
+
+
+  const getData = async (e) => {
+    e.preventDefault();
+    // alert(user);
+
+
+    /////////////////////
+
+    await axios.get(`https://api.github.com/users/${user}`)
+      .then(
+        (res) => {
+          console.log(res);
+          setRecord(res.data);
+        }
+      )
+      .catch((error) => {
+        if (error.response.status === 404) {
+          alert('User not found.');
+        }
+      });
+
+    ///////////////////
+
+    await axios.get(`https://api.github.com/users/${user}/repos`, {
+      params: {
+        per_page: 4,
+        sort: 'updated'
+      }
+    })
+      .then(
+        (res) => {
+          setRepos(res.data);
+        }
+      )
+
+  }
+
+
+
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+
+
+
+
+      {/* <button onClick={getData}>Button</button> */}
+
+
+
+      {/* {record.login} */}
+
+      <Container>
+        <Row className="justify-content-md-center">
+
+
+          <Row className="align-items-center">
+
+
+            <Col md="4">
+
+              <form className='post__commentBox'>
+                <input className='post__input' type="text"
+                  placeholder="Username"
+                  onChange={(e) => setUser(e.target.value)}
+                />
+
+                <button className="post__button"
+                  // disabled={!comment}
+                  // type="submit"
+                  onClick={getData}
+                >
+                  Search
+                </button>
+
+              </form>
+
+
+              {!record.login ? (<h1></h1>) : (
+
+              <div className="users">
+
+                <div>
+                  <img className="users__image" src={record.avatar_url} />
+                </div>
+
+                Username - {record.login}
+                <br />
+                Followers - {record.followers}
+                <br />
+                Repositories Count - {record.public_repos}
+
+
+              </div>
+
+  )
+  }
+
+              {repos &&
+                repos.map((item) => {
+                  return (
+
+                    <div key={item.id} className="repos">
+
+                     
+
+                      Repository - {item.name}
+                      <br />
+                      Stars - {item.stargazers_count}
+                      <br />
+                      Forks - {item.forks}
+
+
+                    </div>
+
+                  );
+                })}
+
+
+              
+
+            </Col>
+
+          </Row>
+
+
+
+
+
+
+
+        </Row>
+
+      </Container>
+
+
+
+
+
     </div>
   );
 }
